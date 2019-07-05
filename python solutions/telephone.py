ef553e30efc010a701a37de2e95011552bfcd926
Kevin Bebel <kevin@rapidtrade.biz>
2019-07-06 00:09:38 +0200

    new solution

---
+++ python solutions/telephone.py
@@ -0,0 +1,6 @@
+def create_phone_number(n):
+    #your code here
+    print(n)
+    s = "({}) {}-{}"
+    n = list(map(lambda item : str(item),n))
+    return s.format( "".join(n[0:3]), "".join(n[4:7]), "".join(n[-4:]) )
